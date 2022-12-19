98e5190d1e823d45f15face2c64bdbfd32c16f02
Maroonest8985 <90228118+Maroonest8985@users.noreply.github.com>
2022-12-19 17:16:50 +0900

    Initial commit

---
+++ src/main/java/com/maroonest/firstjenkins/FirstJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.maroonest.firstjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstJenkinsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstJenkinsApplication.class, args);
+    }
+
+}
