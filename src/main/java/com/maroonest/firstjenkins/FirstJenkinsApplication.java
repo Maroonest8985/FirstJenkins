b9fa375a940e61d655b0996c06132385448925aa
Maroonest8985 <90228118+Maroonest8985@users.noreply.github.com>
2022-12-19 18:00:45 +0900

    security disable

--- src/main/java/com/maroonest/firstjenkins/FirstJenkinsApplication.java
+++ src/main/java/com/maroonest/firstjenkins/FirstJenkinsApplication.java
@@ -2,8 +2,9 @@ package com.maroonest.firstjenkins;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class FirstJenkinsApplication {
 
     public static void main(String[] args) {
