b48c854ba27856f50b1d3d420971b3b5a0717205
Maroonest8985 <90228118+Maroonest8985@users.noreply.github.com>
2023-07-03 15:24:30 +0900

    controller add

---
+++ src/main/java/com/maroonest/firstjenkins/api/controller/LoginController.java
@@ -0,0 +1,16 @@
+package com.maroonest.firstjenkins.api.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@RestController
+public class LoginController {
+
+    @GetMapping("/login")
+    public String LoginController(String id, String passwd){
+        return "Success";
+    }
+}
+
