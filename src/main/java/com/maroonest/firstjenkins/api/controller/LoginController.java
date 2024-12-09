df642df5185c72845b2166f9bcd636fe290bb2be
maroonest <maroonest98@efit.co.kr>
2024-12-09 11:30:43 +0900

    Get -> POST

--- src/main/java/com/maroonest/firstjenkins/api/controller/LoginController.java
+++ src/main/java/com/maroonest/firstjenkins/api/controller/LoginController.java
@@ -2,13 +2,14 @@ package com.maroonest.firstjenkins.api.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 @RestController
 public class LoginController {
 
-    @GetMapping("/login")
+    @PostMapping("/login")
     public String LoginController(String id, String passwd){
         return "Su ccess";
     }
