cf7dfb966fed0bfa826fc426f5702ec13ea1c0d9
maroonest <maroonest98@efit.co.kr>
2024-12-09 11:38:40 +0900

    VO

--- src/main/java/com/maroonest/firstjenkins/api/controller/LoginController.java
+++ src/main/java/com/maroonest/firstjenkins/api/controller/LoginController.java
@@ -3,15 +3,17 @@ package com.maroonest.firstjenkins.api.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import vo.LoginVo;
 
 @Controller
 @RestController
 public class LoginController {
 
     @PostMapping("/login")
-    public String LoginController(String id, String passwd){
-        return "Su ccess";
+    public String LoginController(@RequestBody LoginVo loginVo) {
+        return loginVo.getUsername() + ":" + loginVo.getPassword();
     }
 }
 
