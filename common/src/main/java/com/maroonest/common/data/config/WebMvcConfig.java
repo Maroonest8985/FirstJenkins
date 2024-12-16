26df68e300c360f85f01cdf810bc8f31a0cdde26
maroonest <maroonest98@efit.co.kr>
2024-12-16 17:13:58 +0900

    CORS

--- common/src/main/java/com/maroonest/common/data/config/WebMvcConfig.java
+++ common/src/main/java/com/maroonest/common/data/config/WebMvcConfig.java
@@ -11,7 +11,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://http://138.2.62.193")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true)
                 .maxAge(3600);
