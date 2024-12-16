23d603eedc63dd9bbaa9a4105fe5c363e4b42cd4
maroonest <maroonest98@efit.co.kr>
2024-12-16 14:23:15 +0900

    CORS

--- service/auth/src/main/java/com/maroonest/auth/api/controller/LoginController.java
+++ service/auth/src/main/java/com/maroonest/auth/api/controller/LoginController.java
@@ -3,6 +3,7 @@ package com.maroonest.auth.api.controller;
 import com.maroonest.common.data.LoginVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class LoginController {
 
+    @CrossOrigin(origins="*")
     @PostMapping("/login")
     public String Login(@RequestBody LoginVO loginVO) {
         log.info("SUCCEED");
