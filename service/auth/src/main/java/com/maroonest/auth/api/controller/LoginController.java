275c51f4c19b9f71d06bf1275fdc3aa37760c199
maroonest <maroonest98@efit.co.kr>
2024-12-16 13:29:10 +0900

    LOG add

--- service/auth/src/main/java/com/maroonest/auth/api/controller/LoginController.java
+++ service/auth/src/main/java/com/maroonest/auth/api/controller/LoginController.java
@@ -1,17 +1,22 @@
 package com.maroonest.auth.api.controller;
 
 import com.maroonest.common.data.LoginVO;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @Controller
 @RestController
 public class LoginController {
 
     @PostMapping("/login")
     public String Login(@RequestBody LoginVO loginVO) {
+        log.info("SUCCEED");
+
+
         return loginVO.getUsername() + " : " + loginVO.getPassword();
     }
 }
