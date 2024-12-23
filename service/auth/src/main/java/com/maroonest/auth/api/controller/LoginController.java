440b6a636878c08225b5591a8207f3fc4fc4983e
maroonest <maroonest98@efit.co.kr>
2024-12-23 13:33:00 +0900

    API 주소변경

--- service/auth/src/main/java/com/maroonest/auth/api/controller/LoginController.java
+++ service/auth/src/main/java/com/maroonest/auth/api/controller/LoginController.java
@@ -3,21 +3,18 @@ package com.maroonest.auth.api.controller;
 import com.maroonest.common.data.LoginVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @Controller
-@RestController
+@RestController(value = "/api/v1/auth")
 public class LoginController {
 
     @PostMapping("/login")
     public LoginVO Login(@RequestBody LoginVO loginVO) {
         log.info("SUCCEED");
-
-
 
         return loginVO;
     }
